63ae7bacd96cc752c748604f3b07ca0190a652db
blackpegasusdg <dipghosh28032005@gmail.com>
2026-08-30 10:37:22 +0530

    ui: add Neo-Walkman spacing tokens

---
+++ app/src/main/java/com/dip/a10swalkman/ui/theme/WalkmanSpacing.kt
@@ -0,0 +1,15 @@
+package com.dip.a10swalkman.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+/** Consistent spacing tokens for the Walkman interface. */
+object WalkmanSpacing {
+    val xxs = 4.dp
+    val xs = 8.dp
+    val sm = 12.dp
+    val md = 16.dp
+    val lg = 24.dp
+    val xl = 32.dp
+    val xxl = 48.dp
+    val screen = 20.dp
+}
